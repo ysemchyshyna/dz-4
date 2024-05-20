88f4b17b4dd611cfe75d2aba11eb1609b3e08de7
ysemchyshyna <yuliia.semchyshyna@laba.academy>
2024-05-20 17:12:02 +0300

    dz4

---
+++ src/main/java/org/example/Bag.java
@@ -0,0 +1,12 @@
+package org.example;
+
+/**
+ * Hello world!
+ *
+ */
+public class Bag {
+    public static void main(String[] args) {
+        Fruit Orange = new Fruit();
+        System.out.println(Orange.getSize());
+    }
+}
---
+++ src/main/java/org/example/Fruit.java
@@ -0,0 +1,16 @@
+package org.example;
+
+public class Fruit {
+    public int size = 5;
+
+    Fruit() {
+    }
+
+    Fruit(int size) {
+        this.size = size;
+    }
+
+    public int getSize() {
+        return size;
+    }
+}
